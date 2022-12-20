8074e0e2f9b7da67b7a66cdc65265e384c1ec446
alaxhenry <alaxhenry0121@gmail.com>
2022-12-20 16:36:11 +0900

    Initial Commit

---
+++ FlightAppUI/FlightAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAppUIApp.swift
+//  FlightAppUI
+//
+//  Created by Seungchul Ha on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct FlightAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
